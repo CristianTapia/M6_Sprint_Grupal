eb2b9e1c0c3bffe67800750325674ad1bba3e036
Cristian <ctapia.macaya@gmail.com>
2023-03-10 20:57:30 -0300

    actualizado

--- app.js
+++ app.js
@@ -6,12 +6,37 @@ const fs = require('fs');
 // Crear servidor express
 const app = express();
 
-// Permitir archivos estaticos (CSS)
-app.use(express.static('public'))
-
 // Configurar el servidor para usar el motor hbs
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
+
+app.get('/resultados', (req, res) => {
+    // Obtener datos del archivo JSON
+    fs.readFile('carrera.json', (err, data) => {
+        let dataParsed = JSON.parse(data);
+        console.log(dataParsed);
+        res.render('resultados', { dataCard: dataParsed.carrera});
+    });
+
+    // Obtener datos del archivo JSON
+    fs.readFile('equipos.json', (err, data) => {
+        let dataParsed = JSON.parse(data);
+        console.log(dataParsed);
+        res.render('puntajes', { dataCard: dataParsed.equipos});
+    });
+     
+})
+
+app.get('/puntajes', (req, res) => {
+   // Obtener datos del archivo JSON
+    fs.readFile('equipos.json', (err, data) => {
+        let dataParsed = JSON.parse(data);
+        console.log(dataParsed);
+        res.render('puntajes', { dataCard: dataParsed.equipos});
+    });
+})
+
+
 
 // Hacer que la app escuche el puerto 3000
 app.listen('3000', () => {
