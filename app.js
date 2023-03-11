916d1a4f9ef349091023ab508b82ae9a023615a7
Cristian <ctapia.macaya@gmail.com>
2023-03-10 21:21:59 -0300

    modificacion json equipos

--- app.js
+++ app.js
@@ -27,17 +27,6 @@ app.get('/resultados', (req, res) => {
      
 })
 
-app.get('/puntajes', (req, res) => {
-   // Obtener datos del archivo JSON
-    fs.readFile('equipos.json', (err, data) => {
-        let dataParsed = JSON.parse(data);
-        console.log(dataParsed);
-        res.render('puntajes', { dataCard: dataParsed.equipos});
-    });
-})
-
-
-
 // Hacer que la app escuche el puerto 3000
 app.listen('3000', () => {
     console.log('Servicio levantado')
